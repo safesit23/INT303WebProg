f9a807c753fe54253e4528d9b1a8197cadfdfa99
INT303 <INT303@TRAIN1_2-PC38.ad.sit.kmutt.ac.th>
2018-08-08 09:41:53 +0700

    [ADD] number to test

--- KeyPrimeNumber/src/java/test/testPrimeNumber.java
+++ KeyPrimeNumber/src/java/test/testPrimeNumber.java
@@ -4,7 +4,10 @@ import model.PrimeNumber;
 
 public class testPrimeNumber {
     public static void main(String[] args) {
-        PrimeNumber pn = new PrimeNumber(19);
-        System.out.println("CHECK: "+pn.isPrimeNumber());
+        PrimeNumber pn = new PrimeNumber();
+        pn.setNumber(2);
+        System.out.printf("Is %d is Prime Number ?  %b\n",pn.getNumber(),pn.isPrimeNumber());
+        pn.setNumber(3);
+        System.out.printf("Is %d is Prime Number ?  %b\n",pn.getNumber(),pn.isPrimeNumber());
     }
 }
