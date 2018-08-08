1ca1d94c648f2c52d716b482d5813a18641dbf87
INT303 <INT303@TRAIN1_2-PC38.ad.sit.kmutt.ac.th>
2018-08-08 10:18:33 +0700

    [ADD] Servlet (Controller)

--- KeyPrimeNumber/src/java/servlet/PrimeNumberServlet.java
+++ KeyPrimeNumber/src/java/servlet/PrimeNumberServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.PrimeNumber;
 
 /**
  *
@@ -29,19 +30,11 @@ public class PrimeNumberServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PrimeNumberServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PrimeNumberServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        String n = request.getParameter("number");
+        int number = Integer.valueOf(n);
+        PrimeNumber pn = new PrimeNumber(number);
+        request.setAttribute("pn", pn);
+        getServletContext().getRequestDispatcher("/PrimeNumberView.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
