470c8877a431c3747ab9b620ed1b42c270f4e4bb
INT303 <INT303@TRAIN1_2-PC38.ad.sit.kmutt.ac.th>
2018-08-08 16:47:43 +0700

    [Edit]

--- MyFirstWebApplication/src/java/sit/int303/first/servlet/ProductListServlet.java
+++ MyFirstWebApplication/src/java/sit/int303/first/servlet/ProductListServlet.java
@@ -40,6 +40,7 @@ public class ProductListServlet extends HttpServlet {
         request.setAttribute("products", products);
         getServletContext().getRequestDispatcher("/ProductList.jsp").forward(request, response);
     }
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
