12c149e197c6b8b3a693885db6d4b362efd6ff18
INT303 <INT303@TRAIN1_2-PC38.ad.sit.kmutt.ac.th>
2018-08-08 09:35:14 +0700

    [ADD] Checking PrimeNumber

---
+++ KeyPrimeNumber/src/java/model/PrimeNumber.java
@@ -0,0 +1,32 @@
+package model;
+
+public class PrimeNumber {
+    private int number;
+
+    public PrimeNumber() {
+    }
+
+    public PrimeNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    
+    public boolean isPrimeNumber(){
+        boolean isPrime = number > 1?true:false;    //ถ้าตัวเลขมากกว่า 1 ให้ตั้งต้นเป็น true
+        for (int i = 2; i < number/2; i++) {        //เช็คเพียงครึ่งเดียวว่าหารลงตัวไหม
+            if(number%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+    
+}
