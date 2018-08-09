38fbd27881dfdf17ae9a446ad2cb74ae1c17ac5e
INT303 <INT303@TRAIN1_2-PC38.ad.sit.kmutt.ac.th>
2018-08-09 10:27:13 +0700

    [EDIT] Use session (replace request)

--- MyFirstWebApplication/src/java/sit/int303/first/servlet/PrimeNumberServlet.java
+++ MyFirstWebApplication/src/java/sit/int303/first/servlet/PrimeNumberServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import sit.int303.first.model.PrimeNumber;
 
 /**
@@ -30,17 +31,21 @@ public class PrimeNumberServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        
+        //Add session
+        HttpSession session = request.getSession(true);
         
         String n = request.getParameter("number");
         //ถ้าตัวเลขไม่ null จะ set Attribute
         if (n != null) {
             int number = Integer.valueOf(n);
-            PrimeNumber pn = new PrimeNumber(number);
-            request.setAttribute("pn", pn);
+            //ส่งไปเป้ฯ object รับกลับมาต้อง down casting
+            PrimeNumber pn = (PrimeNumber)session.getAttribute("pn");
+            if(pn==null){
+                pn = new PrimeNumber(number);
+                session.setAttribute("pn", pn);
+            }
+            pn.setNumber(number);
         }
-
         getServletContext().getRequestDispatcher("/PrimeNumberView.jsp").forward(request, response);
     }
 
