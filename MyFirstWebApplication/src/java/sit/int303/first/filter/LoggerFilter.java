e85f593ef6f7dcc2fd95451f955d75e9a50adeb5
INT303 <INT303@TRAIN1_2-PC38.ad.sit.kmutt.ac.th>
2018-09-07 10:08:10 +0700

    [ADD] Logger Filter

---
+++ MyFirstWebApplication/src/java/sit/int303/first/filter/LoggerFilter.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.int303.first.filter;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author INT303
+ */
+
+public class LoggerFilter implements Filter {
+
+    private FilterConfig filterConfig;
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    
+    }
+
+    @Override
+    public void destroy() {
+    
+    }
+    
+}
+
