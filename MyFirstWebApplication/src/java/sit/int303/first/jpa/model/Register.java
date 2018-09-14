ae1cbe9bb871514ff4abe656961cc030bda09fd5
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-14 10:40:57 +0700

    [ADD] register JPA

---
+++ MyFirstWebApplication/src/java/sit/int303/first/jpa/model/Register.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.int303.first.jpa.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+@Entity
+@Table(name = "REGISTER")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Register.findAll", query = "SELECT r FROM Register r")
+    , @NamedQuery(name = "Register.findByEmail", query = "SELECT r FROM Register r WHERE r.email = :email")
+    , @NamedQuery(name = "Register.findByPassword", query = "SELECT r FROM Register r WHERE r.password = :password")
+    , @NamedQuery(name = "Register.findByActivatekey", query = "SELECT r FROM Register r WHERE r.activatekey = :activatekey")
+    , @NamedQuery(name = "Register.findByRegdate", query = "SELECT r FROM Register r WHERE r.regdate = :regdate")
+    , @NamedQuery(name = "Register.findByActivatedate", query = "SELECT r FROM Register r WHERE r.activatedate = :activatedate")})
+public class Register implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "EMAIL")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 40)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 40)
+    @Column(name = "ACTIVATEKEY")
+    private String activatekey;
+    @Column(name = "REGDATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date regdate;
+    @Column(name = "ACTIVATEDATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date activatedate;
+
+    public Register() {
+    }
+
+    public Register(String email) {
+        this.email = email;
+    }
+
+    public Register(String email, String password, String activatekey) {
+        this.email = email;
+        this.password = password;
+        this.activatekey = activatekey;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getActivatekey() {
+        return activatekey;
+    }
+
+    public void setActivatekey(String activatekey) {
+        this.activatekey = activatekey;
+    }
+
+    public Date getRegdate() {
+        return regdate;
+    }
+
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+
+    public Date getActivatedate() {
+        return activatedate;
+    }
+
+    public void setActivatedate(Date activatedate) {
+        this.activatedate = activatedate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (email != null ? email.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Register)) {
+            return false;
+        }
+        Register other = (Register) object;
+        if ((this.email == null && other.email != null) || (this.email != null && !this.email.equals(other.email))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "sit.int303.first.jpa.model.Register[ email=" + email + " ]";
+    }
+    
+}
---
+++ MyFirstWebApplication/src/java/sit/int303/first/jpa/model/controller/RegisterJpaController.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sit.int303.first.jpa.model.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+import sit.int303.first.jpa.model.Register;
+import sit.int303.first.jpa.model.controller.exceptions.NonexistentEntityException;
+import sit.int303.first.jpa.model.controller.exceptions.PreexistingEntityException;
+import sit.int303.first.jpa.model.controller.exceptions.RollbackFailureException;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class RegisterJpaController implements Serializable {
+
+    public RegisterJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Register register) throws PreexistingEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            em.persist(register);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            if (findRegister(register.getEmail()) != null) {
+                throw new PreexistingEntityException("Register " + register + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Register register) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            register = em.merge(register);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                String id = register.getEmail();
+                if (findRegister(id) == null) {
+                    throw new NonexistentEntityException("The register with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(String id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Register register;
+            try {
+                register = em.getReference(Register.class, id);
+                register.getEmail();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The register with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(register);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Register> findRegisterEntities() {
+        return findRegisterEntities(true, -1, -1);
+    }
+
+    public List<Register> findRegisterEntities(int maxResults, int firstResult) {
+        return findRegisterEntities(false, maxResults, firstResult);
+    }
+
+    private List<Register> findRegisterEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Register.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Register findRegister(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Register.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getRegisterCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Register> rt = cq.from(Register.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
