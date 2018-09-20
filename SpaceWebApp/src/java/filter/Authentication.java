114813e0a6722cf8036eda5d35f4611093d165a6
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-20 16:57:33 +0700

    [ADD] Filter

---
+++ SpaceWebApp/src/java/filter/Authentication.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filter;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+@WebFilter(filterName = "Authentication", servletNames = {"WeightConverterServlet"}, dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
+public class Authentication implements Filter {
+    private FilterConfig config;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.config = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest) request).getSession(false);
+        if(session==null || session.getAttribute("astronomer") ==null){
+            config.getServletContext().getRequestDispatcher("/Login").forward(request, response);
+        }else{
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
