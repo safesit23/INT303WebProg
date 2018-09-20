87250cb9aa95718ac84adb0841403c86928d30bb
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-20 22:19:33 +0700

    [UPDATE] delete import

--- SpaceWebApp/src/java/servlet/LogoutServlet.java
+++ SpaceWebApp/src/java/servlet/LogoutServlet.java
@@ -6,7 +6,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
