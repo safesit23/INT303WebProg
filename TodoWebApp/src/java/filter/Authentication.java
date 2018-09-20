9535c610df497f4465963c541cf1d836d29e1045
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-09-20 08:47:54 +0700

    [UPDATE] add condition to check session
    Warning use " || " only ! At First I use &&, Then Error 505, I think because it cannot find Attribute("account")

--- TodoWebApp/src/java/filter/Authentication.java
+++ TodoWebApp/src/java/filter/Authentication.java
@@ -6,15 +6,14 @@
 package filter;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -25,12 +24,17 @@ public class Authentication implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.filterConfig = filterConfig;
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        HttpSession session = ((HttpServletRequest)request).getSession(false);
+        if(session ==null || session.getAttribute("account") == null){
+            filterConfig.getServletContext().getRequestDispatcher("/Login").forward(request, response);
+            return;
+        }
+        chain.doFilter(request, response);
     }
 
     @Override
