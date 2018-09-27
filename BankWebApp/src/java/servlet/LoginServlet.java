a914ccdebf5a26b13100a0882799964c7250c637
safesit23 <jatawatsafe@hotmail.com>
2018-09-27 17:36:28 +0700

    [UPDATE] add condition

--- BankWebApp/src/java/servlet/LoginServlet.java
+++ BankWebApp/src/java/servlet/LoginServlet.java
@@ -6,18 +6,27 @@
 package servlet;
 
 import java.io.IOException;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+import model.Account;
+import model.controller.AccountJpaController;
 
 /**
  *
  * @author jatawatsafe
  */
 public class LoginServlet extends HttpServlet {
-
+@PersistenceUnit (unitName = "BankWebAppPU")
+EntityManagerFactory emf;
+@Resource
+UserTransaction utx;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -33,11 +42,25 @@ public class LoginServlet extends HttpServlet {
         String idText = request.getParameter("id");
         String pinText = request.getParameter("pin");
         if(idText != null && pinText !=null){
-            
-            getServletContext().getRequestDispatcher("/MyAccount.jsp").forward(request, response);
-        }else{
-            getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+            int id = Integer.parseInt(idText);
+            int pin = Integer.parseInt(pinText);
+            String loginmessage = null;
+            AccountJpaController aCtrl = new AccountJpaController(utx, emf);
+            Account account = aCtrl.findAccount(id);
+            if(account != null){
+                if(account.getPin()==pin){
+                    session.setAttribute("account", account);
+                    getServletContext().getRequestDispatcher("/MyAccount.jsp").forward(request, response);
+                }else{
+                     loginmessage = "Pin Invalid !";
+                }
+            }else{
+                loginmessage = "Account Id Invalid !";
+            }
+            request.setAttribute("loginmessage", loginmessage);
         }
+        getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
