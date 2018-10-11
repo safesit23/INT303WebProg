ffcea048afc08083b9adeabca9869b0ce9c141a6
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2018-10-11 16:11:10 +0700

    [ADD] WithdrawServlet

---
+++ BankWebApp/src/java/servlet/WithdrawServlet.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+import model.Account;
+import model.History;
+import model.controller.AccountJpaController;
+import model.controller.HistoryJpaController;
+import model.controller.exceptions.NonexistentEntityException;
+import model.controller.exceptions.RollbackFailureException;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class WithdrawServlet extends HttpServlet {
+
+    @PersistenceUnit(unitName = "BankWebAppPU")
+    EntityManagerFactory emf;
+
+    @Resource
+    UserTransaction utx;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        String withdrawText = request.getParameter("withdraw");
+        Account accSession = (Account) session.getAttribute("account");
+        if (withdrawText != null) {
+            int withdraw = Integer.parseInt(withdrawText);
+            String message = null;
+            AccountJpaController aCtrl = new AccountJpaController(utx, emf);
+            HistoryJpaController hCtrl = new HistoryJpaController(utx, emf);
+            Account account = aCtrl.findAccount(accSession.getAccountid());
+            if (account != null) {
+                boolean checkDo = account.withdraw(withdraw);
+                if (checkDo) {
+                    History history = new History(account, "withdraw", withdraw, new Date(), account.getBalance());
+                    try {
+                        aCtrl.edit(account);
+                        hCtrl.create(history);
+                        message = "Withdraw Successful";
+                        session.setAttribute("account", account);
+                    } catch (NonexistentEntityException ex) {
+                        Logger.getLogger(DepositServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (RollbackFailureException ex) {
+                        Logger.getLogger(DepositServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (Exception ex) {
+                        Logger.getLogger(DepositServlet.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    message = "Withdraw Unsuccessful";
+                }
+                request.setAttribute("message", message);
+            }
+            response.sendRedirect("MyAccount.jsp");
+            return;
+        }
+        getServletContext().getRequestDispatcher("/Withdraw.jsp").forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
